6c8201db6fd0330f34c4721ac5ef2b7b6beff46a
Doomz <doomz.online@gmail.com>
2025-12-19 19:03:16 +0200

    transfer is_active component to switch (toggle)

--- resources/js/components/form/is-active.tsx
+++ resources/js/components/form/is-active.tsx
@@ -1,28 +1,16 @@
 import React, { useState } from 'react'
-import { Checkbox } from '../ui/checkbox';
-import { Label } from '../ui/label';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
 
 const IsActive = ({ value }: { value: boolean }) => {
     const [isActive, setIsActive] = useState(value);
     return (
         <div className="flex items-center space-x-2">
-            <Label
-                htmlFor="is_active"
-                className="text-sm font-normal cursor-pointer"
-            >
+            <Label htmlFor="is_active" className="text-sm font-normal">
                 Active
             </Label>
-            <Checkbox
-                id="is_active"
-                name="is_active"
-                defaultChecked={isActive}
-                onCheckedChange={(checked) => setIsActive(checked === true)}
-            />
-            <input
-                type="hidden"
-                name="is_active"
-                value={isActive ? '1' : '0'}
-            />
+            <Switch id="is_active" name="is_active" defaultChecked={isActive} onCheckedChange={(checked) => setIsActive(checked)} className=' cursor-pointer' />
+            <input type="hidden" name="is_active" value={isActive ? '1' : '0'} />
         </div>
     )
 }
